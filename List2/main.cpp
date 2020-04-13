cfa261641a163f31ac0988a2c262e7ffb2b962e4
Smak80 <smak-80@yandex.ru>
2020-04-13 10:03:26 +0300

    Добавьте файлы проекта.

---
+++ List2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	system("pause");
+}
